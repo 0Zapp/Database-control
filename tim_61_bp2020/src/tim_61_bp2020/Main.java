9d82475ead973664017a093919b08315fbd05d0b
Marko Nedeljkovic <marko65734@gmail.com>
2020-05-24 07:20:33 +0200

    Testiranje commitova u eclipsi, primer 2 sa vezbi

--- tim_61_bp2020/src/tim_61_bp2020/Main.java
+++ tim_61_bp2020/src/tim_61_bp2020/Main.java
@@ -1,10 +1,19 @@
 package tim_61_bp2020;
 
+import javax.swing.SwingUtilities;
+
+
 public class Main {
 
 	public static void main(String[] args) {
+		SwingUtilities.invokeLater(new Runnable() {
 
-		System.out.println("Hello World!");
+			@Override
+			public void run() {
+				MainFrame mf = new MainFrame();
+				mf.setVisible(true);
+			}
+		});
 
 	}
 
